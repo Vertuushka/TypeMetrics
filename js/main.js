b0b66d7e7cf8693f771692758d135c738384ba6a
Vertushka <kirillyasniy@gmail.com>
2025-02-01 18:38:18 +0100

    custom components in JS start

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
 // IMPORTANT! the "language" and "night_mode" should always be the last one in this array
 let storage_keys = ["casing", "save_score", "animations", "night_mode"];
 
-let checkbox_unchecked_url;
-let checkbox_checked_url;
+let checkbox_unchecked_url  = "../img/unchecked_light.svg";
+let checkbox_checked_url    = "../img/checked_light.svg";
 
-let night_mode_unchecked_url;
-let night_mode_checked_url;
-
-let storage_handler;
-
-class Storage_handler {
-    constructor(storage_keys) {
-        storage_keys.forEach(key => {
-            if (localStorage.getItem(key) == null)
-                this.set_value(key, false);
-            this[key] = localStorage.getItem(key);
-        });
-    }
-
-    get_value(key) {
-        return localStorage.getItem(key);
-    }
-
-    set_value(key, value) {
-        localStorage.setItem(key, value);
-        this[key] = localStorage.getItem(key);
-    }
-}
+let night_mode_unchecked_url    = "../img/night_mode_checked.svg";
+let night_mode_checked_url      = "../img/night_mode_unchecked.svg";
 
 class Custom_Checkbox {
     constructor(input_element, label_element, img_element, storage_key) {
@@ -35,14 +14,14 @@ class Custom_Checkbox {
         this.image = img_element;
         this.storage_key = storage_key;
 
-        this.set_initial_state(storage_handler[storage_key]);
+        this.set_initial_state(localStorage.getItem(this.storage_key));
 
         this.toggle_checkbox = this.toggle_checkbox.bind(this);
         this.element.addEventListener("change", this.toggle_checkbox);
     }
 
     set_initial_state(state){
-        if (state == "true") {
+        if (state === "true") {
             this.element.checked = true;
             this.toggle_checkbox(true);
         }
@@ -56,8 +35,8 @@ class Custom_Checkbox {
             this.image.src = checkbox_unchecked_url;
         }
         if (visual_only == false) {
-            let current_value = JSON.parse(storage_handler.get_value(this.storage_key));
-            storage_handler.set_value(this.storage_key, !current_value);
+            let current_value = this.element.checked;
+            localStorage.setItem(this.storage_key, JSON.stringify(current_value));
         }
         
     }
@@ -69,39 +48,92 @@ class Night_Mode extends Custom_Checkbox {
     }
 
     toggle_checkbox(object, visual_only=false) {
-        if (this.element.checked) {
-            this.image.src = night_mode_checked_url;
-        }
-        else {
+        if (this.element.checked == true)
             this.image.src = night_mode_unchecked_url;
-        }
+        else
+            this.image.src = night_mode_checked_url;
         if (visual_only == false) {
-            let current_value = JSON.parse(storage_handler.get_value(this.storage_key));
-            storage_handler.set_value(this.storage_key, !current_value);
+            let current_value = this.element.checked;
+            localStorage.setItem(this.storage_key, JSON.stringify(current_value));
         }
-        
     }
 }
 
+class Custom_Select {
+    constructor(select_prefix) {
+        this.select = document.querySelector(`.${select_prefix}_select`);
+        this.icon = document.querySelector(`.${select_prefix}_select_icon`);
+        this.label = document.querySelector(`.${select_prefix}_select_label`);
+        this.option = document.querySelector(`.${select_prefix}_options`)
+        this.options = document.querySelectorAll(`.${select_prefix}_option`);
+
+        this.toggle_options = this.toggle_options.bind(this);
+        this.select.addEventListener("click", this.toggle_options)
+    }
+    
+
+    toggle_options() {
+        this.option.classList.toggle("hidden");
+        if(this.option.classList.contains("hidden")) 
+            this.icon.style.transform = "rotate(0deg)";
+        else
+            this.icon.style.transform = "rotate(180deg)";
+    }
+}
+
+function toggle_visibility(element) {
+    element.classList.toggle("hidden");
+}
+
+function select_onChange(element, class_prefix) {
+    let select_label = document.querySelector(`.${class_prefix}_select_label`);
+    select_label.innerHTML = element.innerHTML;
+
+    let options = document.querySelector(`.${class_prefix}_options`);
+    toggle_visibility(options);
+
+    let select = document.querySelector(`.${class_prefix}_select`);
+    select.dispatchEvent(new Event("change", {bubbles: true}));
+}
+
+function custom_select_toggle_options(options, class_prefix, specialFn=null) {
+    if (specialFn != null) {
+        
+    } else {
+        // let icon = 
+    }
+    toggle_visibility(options);
+}
+
+function setup_custom_select(class_prefix) {
+    let select = document.querySelector(`.${class_prefix}_select`);
+    let options = document.querySelector(`.${class_prefix}_options`);
+    let option = document.querySelectorAll(`.${class_prefix}_option`);
+
+    option.forEach(element => {
+        element.addEventListener("click", () => select_onChange(element, class_prefix))
+    })
+
+    select.addEventListener("click", () => custom_select_toggle_options(options, class_prefix));
+}
+
+function text_change() {
+    console.log("text was changed!")
+}
+
 function document_init() {
-    checkbox_unchecked_url = "../img/unchecked_light.svg";
-    checkbox_checked_url = "../img/checked_light.svg";
 
-    night_mode_checked_url = "../img/night_mode_checked.svg";
-    night_mode_unchecked_url = "../img/night_mode_unchecked.svg";
-
-    // IMPORTANT only simple true-false circle checkboxes should be selected!
     let custom_checkboxes = document.querySelectorAll(".custom_checkbox");
     let custom_labels = document.querySelectorAll(".custom_checkbox_label");
     let custom_images = document.querySelectorAll(".custom_checkbox_img");
-
-    storage_handler = new Storage_handler(storage_keys);
 
     custom_checkboxes.forEach((element, index) => {
         new Custom_Checkbox(element, custom_labels[index], custom_images[index], storage_keys[index]);
     });
 
-    new Night_Mode(document.querySelector("#color_scheme"), document.querySelector("#color_scheme_label"), document.querySelector("#color_scheme_img"));
+    new Custom_Select("text");
+
+    new Night_Mode(document.querySelector("#color_scheme"), document.querySelector("#color_scheme_label"), document.querySelector("#color_scheme_img"), "night_mode");
 }
 
 document.addEventListener("DOMContentLoaded", document_init)
